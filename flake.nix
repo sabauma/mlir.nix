5f5f2657e7f549347bced853e81e0bd1b1b5d777
Spenser Bauman <sbauman@mathworks.com>
2024-02-22 14:24:39 -0500

    Reformat

--- flake.nix
+++ flake.nix
@@ -12,8 +12,7 @@
 
   inputs.utils.url = "github:numtide/flake-utils";
 
-  outputs = inputs@{ self, nixpkgs, llvm-project, ... }:
-    inputs.utils.lib.eachSystem [
+  outputs = inputs@{ self, nixpkgs, llvm-project, ... }: inputs.utils.lib.eachSystem [
       "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"
     ] (system: let
       pkgs = import nixpkgs { inherit system; };
