9f975456446b1c6cbdfe997de610585665567ff5
Spenser Bauman <sabauma@fastmail>
2024-06-13 08:10:16 -0400

    Take 2

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"
     ] (system: let
       pkgs = import nixpkgs { inherit system; };
-    in {
+    in rec {
       packages.mlir = pkgs.stdenv.mkDerivation {
         name = "llvm-mlir";
 
